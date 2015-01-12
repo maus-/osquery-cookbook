a5123f9a48da50a0f9c0c459d495f757af5d567d
a wizard named upfish <maus.stearns@workday.com>
2015-01-12 14:39:09 -0800

    updated metadata

--- chef/cookbooks/osquery/metadata.rb
+++ chef/cookbooks/osquery/metadata.rb
@@ -1,10 +1,10 @@
 name             'osquery'
 maintainer       'Maus Stearns'
-maintainer_email 'maus.stearns@workday.com'
+maintainer_email ''
 license          'All rights reserved'
 description      'Installs/Configures osquery'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '.2'
 
 
 depends "yum"
